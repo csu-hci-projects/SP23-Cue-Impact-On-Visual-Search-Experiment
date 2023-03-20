c6307b9a9d3de1325d9f755d2291248fbb26b946
Ayden Adair <99846884+Keetchie@users.noreply.github.com>
2023-03-20 11:00:31 -0600

    Timer stops after counter reaches limit

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,22 +10,31 @@ public class Timer : MonoBehaviour
     public TextMeshProUGUI timerText;
     public float currentTime;
     public Image imageComponent;
-    private int counter = 0;
+    private static int counter = 0;
+    private float finalTime;
+    private bool doneTiming = false;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(counter == 10) {
-            
-        } else {
-        currentTime = currentTime += Time.deltaTime;
-        timerText.text = currentTime.ToString("0.00");
+        if(counter == 10 && !doneTiming) {
+            finalTime = currentTime;
+            doneTiming = true;
+            timerText.text = finalTime.ToString("0.00");
+        } else if (!doneTiming) {
+            Debug.Log("Current counter:" + counter);
+            currentTime = currentTime += Time.deltaTime;
+            timerText.text = currentTime.ToString("0.00");
         }
+    }
+
+    public static void UpdateCounter() {
+        counter++;
     }
 }
